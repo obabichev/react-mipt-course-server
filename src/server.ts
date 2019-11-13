e020ea1a9305bd3eb925ae3e32c434afc1897340
obabichev <babichev.oleg.n@gmail.com>
2019-11-13 18:17:00 +0100

    auth

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,13 @@ import 'dotenv/config';
 import App from './app';
 import PostsController from './posts/posts.controller';
 import validateEnv from './utils/validateEnv';
+import AuthenticationController from './authentication/authentication.controller';
 
 validateEnv();
 
 const app = new App(
     [
+        new AuthenticationController(),
         new PostsController(),
     ],
 );
