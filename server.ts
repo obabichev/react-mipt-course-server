a55b85dd6c59ebc7c407e355dc7b5ea0b0f5af5c
obabichev <babichev.oleg.n@gmail.com>
2019-11-20 20:23:21 +0100

    remove validation env

--- server.ts
+++ server.ts
@@ -1,11 +1,8 @@
 import 'dotenv/config';
 import App from './src/app';
 import PostsController from './src/posts/posts.controller';
-import validateEnv from './src/utils/validateEnv';
 import AuthenticationController from './src/authentication/authentication.controller';
 import BoardController from './src/board/board.controller';
-
-validateEnv();
 
 const app = new App(
     [
