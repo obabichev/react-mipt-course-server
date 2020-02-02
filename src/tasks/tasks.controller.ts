8ac2e2464224e69488a767ec8f904b8d1f04a0b7
obabichev <babichev.oleg.n@gmail.com>
2020-02-02 16:04:14 +0100

    find by key or id

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -23,6 +23,7 @@ class TasksController implements Controller {
 
     private initializeRoutes() {
         this.router.get(this.path, this.getAllTasks);
+        this.router.get(`${this.path}/:key`, this.getTaskByKeyOrId);
         this.router
             .all(`${this.path}/*`, authMiddleware)
             .post(this.path, authMiddleware, validationMiddleware(CreateTaskDto), this.createTask);
@@ -70,6 +71,28 @@ class TasksController implements Controller {
 
         response.send(savedTask);
     };
+
+    private getTaskByKeyOrId = async (request: express.Request, response: express.Response, next: express.NextFunction) => {
+        const key = request.params.key;
+        try {
+            const task = await this.task.findById(key);
+            if (task) {
+                return response.send(task);
+            }
+        } catch (e) {
+
+        }
+        try {
+            const task = await this.task.findOne({key});
+            if (task) {
+                return response.send(task);
+            }
+        } catch (e) {
+
+        }
+
+        return next(new HttpException(400, 'Task was not found'));
+    }
 }
 
 export default TasksController;
